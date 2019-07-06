558f2506041136b9737cb8ee7761026df4b32d3f
esdalee <es.da.lee@gmail.com>
2019-07-06 19:26:03 -0400

    updated

--- liri.js
+++ liri.js
@@ -52,28 +52,31 @@ var LiriCommand = function() {
                 var title =  spotifyData.name;
                 var preview = spotifyData.external_urls.spotify;
                 var album = spotifyData.album.name;
-            })
+                // combine all info for movie
+                var songResult = "\nArtist: " + artist + "\nTitle: " + title + "\nLink Preview: " + preview + "\nAlbum: " + album + "\n\n";
+                console.log(songResult);
+                fs.appendFile("log.txt", songResult, function(error) {
+                    if (error) {
+                        console.log(error);
+                    }
+            });
+        })
             .catch(function(error){
             console.log("Error: " + error);
             });
-
-        // artist(s)
-        // song's name
-        // preview link of song
-        // album that song is from
-
     };
 
     this.movie = function(movie) {
         // make axios call to search for movie details
-        var URL = "www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-        axios.get(URL).then(function(response){       
-            var movieData = response.data[0];
-            // console.log(data);
+        var URL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy"
+        axios.get(URL).then(function(response){
+            var movieData = response.data;
+            // console.log(movieData);
             var title = movieData.Title;
             var year = movieData.Year;
             var imdb = movieData.imdbRating;
             var rottenTomatoes = movieData.Ratings[1].Value;
+            console.log(rottenTomatoes);
             var prodCountry = movieData.Country;
             var language = movieData.Language;
             var plot = movieData.plot;
@@ -91,11 +94,9 @@ var LiriCommand = function() {
         console.log(error);
         });
     };
-
-// do-what-it-says
-
 };
 
+// Run functions
 // Create liri object
 var liriCommand = new LiriCommand();
 
@@ -110,18 +111,48 @@ else if (command === "spotify-this-song") {
     if (!term) {
         term = "The Sign";
     }
+    console.log("Looking for song info for " + term)
     liriCommand.spotify(term);
 }
 
 // MOVIE
 else if (command === "movie-this") {
+    // Without specific movie
     if (!term) {
         term = "Mr.Nobody";
-    }
-    term = arg.slice(3).join("+");
-    liriCommand.movie(term);
+        liriCommand.movie(term);
+    } 
+    // Run for user inputed movie
+    else {
+    console.log("Looking for movie info for " + term);
+    var newTerm = arg.slice(3).join("+");
+    // console.log(newTerm);
+    liriCommand.movie(newTerm);
+    };
+}
+
+// DO WHAT IT SAYS
+else if (command === "do-what-it-says") {
+    fs.readFile('random.txt', 'utf8', (error,data) =>{
+        if (error) throw error;
+        console.log(data);
+        var content = data.split(" ");
+        console.log(content);
+        var txtCommand = content[0];
+        var txtTerm = content.slice(1).join(" ");
+        console.log(txtCommand, txtTerm);
+        // Cases depending on command in txt file
+        switch(txtCommand) {
+            case "concert-this":
+                liriCommand.concert(txtTerm);
+                break;
+            case "spotify-this-song":
+                liriCommand.spotify(txtTerm);
+                break;
+            case "movie-this":
+                var newTerm = txtTerm.slice(3).join("+");
+                liriCommand.movie(newTerm);
+                break;
+        };
+    });
 };
-
-// else if (command === "do-what-it-says") {
-
-// };
